d49178f2eb8fb15e0827d19b71ee243b279848e4
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-02-09 22:29:25 +0100

    defined all pages and navbar component

---
+++ resources/js/pages/PagePosts.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container my-5 py-5">
+        <div class="row g-3">
+            <div class="col-sm-6 col-md-4" v-for="post in arrPosts" :key="post.id">
+                <div class="card h-100">
+                    <img :src="'/storage/' + post.uploaded_img" class="card-img-top h-50" :alt="post.title">
+                    <div class="card-body d-flex flex-column">
+                        <h5 class="card-title">{{ post.title }}</h5>
+                        <p class="card-text flex-grow-1">{{ post.excerpt }}</p>
+                        <a href="#" class="btn btn-primary">Leggi il post</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            arrPosts: [],
+        };
+    },
+    created() {
+        axios.get('api/posts')
+            .then(response => this.arrPosts = response.data.results)
+    },
+}
+</script>
+
+<style>
+
+</style>
