2112dd93c43d3311efa284326071b3fd07d6a02d
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-02-09 22:28:48 +0100

    define the routes

--- resources/js/front.js
+++ resources/js/front.js
@@ -2,8 +2,43 @@ require('./bootstrap');
 
 import Vue from 'vue';
 import App from './App.vue';
+import VueRouter from 'vue-router';
+import PageHome from './pages/PageHome';
+import PageAbout from './pages/PageAbout';
+import PagePosts from './pages/PagePosts';
+import PageSinglePost from './pages/PageSinglePost';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: 'pageHome',
+        component: PageHome,
+    },
+    {
+        path: '/about',
+        name: 'pageAbout',
+        component: PageAbout,
+    },
+    {
+        path: '/posts',
+        name: 'pagePosts',
+        component: PagePosts,
+    },
+    {
+        path: '/posts/params',
+        name: 'PageSinglePost',
+        component: PageSinglePost,
+    },
+];
+
+const router = new VueRouter({
+    routes
+});
 
 new Vue({
     el: '#root',
     render: h => h(App),
+    router
 });
